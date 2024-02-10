9e40da4ed4c5440ff76986f477d5e9ca0ec32909
bucr <bucrpp@163.com>
2024-02-10 18:43:13 +0800

    create remember everything project

---
+++ src/main/java/com/bucr/remembereverything/RememberEverythingApplication.java
@@ -0,0 +1,13 @@
+package com.bucr.remembereverything;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RememberEverythingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RememberEverythingApplication.class, args);
+	}
+
+}
